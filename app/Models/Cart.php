175fb3ef0efcc8b5f24e6635ef55071bfa587672
Tom Kuijer <tkuijer@gmail.com>
2021-07-23 20:02:24 +0200

    Add pruning on carts model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,11 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 
 class Cart extends Model
 {
 
+    use Prunable;
+
     protected $guarded = [];
+
+    /**
+     * Get the prunable model query.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function prunable()
+    {
+        return static::where(static::UPDATED_AT, '<=', now()->subWeek());
+    }
+
+    /**
+     * Prepare the model for pruning by detaching associated pizzas.
+     *
+     * @return void
+     */
+    protected function pruning()
+    {
+        $this->pizzas()->detach();
+    }
 
     /**
      * @return Cart
