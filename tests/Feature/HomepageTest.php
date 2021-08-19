0821fec47dcceadaba0fc809ce327e167434c3cd
Tom Kuijer <tkuijer@gmail.com>
2021-08-19 11:48:19 +0200

    Pizza logic tests

---
+++ tests/Feature/HomepageTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Pizza\Pizza;
+use App\Models\Topping;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HomepageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_shows_pizzas_on_the_homepage()
+    {
+        $pizza = $this->createPizza();
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSee($pizza->name);
+    }
+
+    /** @test */
+    function it_shows_pizza_toppings_on_the_homepage()
+    {
+        $toppings = $this->createToppings(2);
+        $pizza = $this->createPizza();
+        $pizza->toppings()->saveMany($toppings);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $toppings->each(function (Topping $topping) use ($response) {
+            $response->assertSee($topping->name);
+        });
+    }
+
+    private function createPizza(string $name = 'Pizza 1'): Pizza
+    {
+        return Pizza::factory()->create([
+            'name' => $name
+        ]);
+    }
+
+    private function createToppings(int $count = 1): Collection
+    {
+        return Topping::factory($count)->create();
+    }
+}
---
+++ tests/Feature/PizzaCrudTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use DB;
+use Tests\TestCase;
+use App\Models\User;
+use App\Pizza\Pizza;
+use App\Models\Topping;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PizzaCrudTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    /** @test */
+    function it_shows_a_listing_of_pizzas()
+    {
+        $pizzas = $this->createPizzas(3);
+
+        $response = $this->actingAs($this->user)
+            ->get(route('pizza.index'));
+
+        $response->assertStatus(200);
+        $pizzas->each(function(Pizza $pizza) use ($response) {
+            $response->assertSee($pizza->name);
+        });
+    }
+
+    /** @test */
+    function it_can_create_a_pizza()
+    {
+        $response = $this->actingAs($this->user)
+            ->post(route('pizza.store'), [
+                'name' => 'Testing Pizza'
+            ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('pizza.edit', 1));
+
+        $this->assertTrue(DB::table('pizzas')->where('name', 'Testing Pizza')->exists());
+    }
+
+    /** @test */
+    function it_can_delete_a_pizza()
+    {
+        $pizza = Pizza::factory()->create([
+            'name' => 'Testing Pizza'
+        ]);
+
+        $response = $this->actingAs($this->user)->delete(route('pizza.destroy', $pizza));
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('pizza.index'));
+        $response->assertSessionHas('success');
+
+        $this->assertFalse(DB::table('pizzas')->where('name', 'Testing Pizza')->exists());
+    }
+
+    /** @test */
+    function it_can_update_a_pizza()
+    {
+        $pizza = Pizza::factory()->create([
+            'name' => 'Testing Pizza'
+        ]);
+
+        $response = $this->actingAs($this->user)
+            ->patch(route('pizza.update', $pizza), [
+                'name' => 'Updated testing pizza'
+            ]);
+
+        $pizza = $pizza->fresh();
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('success');
+        $response->assertRedirect(route('pizza.edit', $pizza));
+
+        $this->assertEquals('Updated testing pizza', $pizza->name);
+    }
+
+    private function createPizzas(int $count = 1): Collection
+    {
+        return Pizza::factory($count)->create();
+    }
+
+    private function createToppings(int $count = 1): Collection
+    {
+        return Topping::factory($count)->create();
+    }
+}
